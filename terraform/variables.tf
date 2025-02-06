9b9e85a0dadf484fd6831aef5f7f29816b6f60de
pjbundalian <57664667+pjbundalian@users.noreply.github.com>
2025-02-07 05:20:46 +0800

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {
+  description = "The ID of the VPC where resources will be deployed."
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "A list of IDs for the public subnets in the VPC."
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "A list of IDs for the private subnets in the VPC."
+  type        = list(string)
+}
