44d2de3d14c987fb452245eed1fa518fe61c418b
pjbundalian <57664667+pjbundalian@users.noreply.github.com>
2025-02-07 05:22:19 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,13 @@ module "vpc" {
 }
 
 module "ec2" {
-  source       = "./ec2"
-  vpc_id       = module.vpc.vpc_id
+  source          = "./ec2"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+}
+
+module "alb" {
+  source         = "./alb"
+  vpc_id         = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnets
 }
